4a3090230cf80f9da173a6cdce5759186d487e6e
phillip.magos <phillipmagos@gmail.com>
2023-06-19 22:21:30 -0500

    update to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,23 +10,26 @@ export default function Contact() {
             <div class="row mt-5 justify-content-center">
               <div class="col-md-6 border border-dark rounded">
                 <h1 class="justify-content-center">Submit Your Questions & Answers Here:</h1>
+                <form action="https:/>/formsubmit.co/phillipmagos@gmail.com" method="POST">
                 <div class="mb-3">
                   <label for="exampleFormControlInput1" class="form-label">First Name</label>
-                  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="First Name"></input>
+                  <input type="text" name="fname" class="form-control" id="exampleFormControlInput1" placeholder="First Name"></input>
                 </div>
                 <div class="mb-3">
                   <label for="exampleFormControlInput1" class="form-label">Last Name</label>
-                  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Last Name"></input>
+                  <input type="text" name="lname" class="form-control" id="exampleFormControlInput1" placeholder="Last Name"></input>
                 </div>
                 <div class="mb-3">
                   <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+                  <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
                 </div>
                 <div class="mb-3">
                   <label for="exampleFormControlTextarea1" class="form-label">Your Inquiry:</label>
-                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  <input type="text" name="inquiry" class="form-control" id="exampleFormControlInput1" rows="3" placeholder="Your Inquiry"></input>
+                  {/* <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea> */}
                   <button class="mt-5">Submit</button>
                 </div>
+                </form>
               </div>
             </div>
           </div>
