5728a51692d9998cfb000adf0dd53511efda6734
phillip.magos <phillipmagos@gmail.com>
2023-06-21 22:26:15 -0500

    updating to node app

---
+++ app.js
@@ -0,0 +1,114 @@
+//
+
+// const http = require('http')
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World! NodeJS \n');
+// });
+
+
+
+// added from https://docs.cpanel.net/knowledge-base/web-services/how-to-install-a-node.js-application/#install-the-application
+
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+app.use(logger('dev'));
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// const mongoose = require('mongoose');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect('/home')
+})
+app.get('/home', (req, res) => {
+  res.render('home.ejs')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio.ejs')
+})
+
+app.get('/blog', (req, res) => {
+  res.render('blog.ejs')
+})
+
+app.get('/projects', (req, res) => {
+  res.render('projects.ejs')
+})
+app.get('/signin', (req, res) => {
+  res.render('signin.ejs')
+})
+
+app.get('/checkout', (req, res) => {
+  res.render('checkout.ejs')
+})
+
+// Beginning of Southpark Characters
+//Bring in our data - How we bring it out object? - Destructure
+const { characters } = require('./fakeData')
+//console.log(characters)
+
+// Use our public folder
+app.use(express.static('public'))
+// Set view engine
+app.set("view engine", "ejs")
+
+// With more than one / in route
+app.use('/character', express.static(__dirname + '/public'))
+
+
+
+// Routes
+app.get('/', (req, res) => {
+    // modify when building req.query to send data
+    // will use this to build our form
+    res.render("home", {data: characters})
+})
+
+// Display all characters
+app.get('/SPCharacters', (req, res) => {
+    res.render("SPCharacters", {data: characters})
+})
+
+// Display a character 
+app.get('/SPCharacter/:id', (req, res) => {
+    // Could say req.params.id - again destructure
+    const { id } = req.params
+    //console.log(characters[id])
+    res.render("SPCharacter", { data: characters[id]})
+})
+
+
+// Display a character using req.query through form
+app.get("/id", (req, res) => {
+    const { id } = req.query
+    res.render("SPCharacter", {data: characters[id]})
+})
+// End of Southpark Characters
+
+//const port = process.env.PORT || 3000
+//app.listen(port, ()=> console.log(`web host on port ${port}`))
+
+// end of added from https://docs.cpanel.net/knowledge-base/web-services/how-to-install-a-node.js-application/#install-the-application
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`phillipmagos.net is listening on port ${port}`));
+// end of app.js
---
+++ fakeData.js
@@ -0,0 +1,26 @@
+exports.characters = [
+	{
+		id: 1,
+		name: "Cartman",
+		img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsuwvlu6gFacg3p5ZzTdtQXGdrMdM3Fw24Ug&usqp=CAU",
+		status: "Alive"
+	},
+	{		
+			id: 2,
+			name: "Kyle",
+			img_url: "http://pa1.narvii.com/6612/c9b35efdbe8cdae3f1348145a8046190a2a2f486_00.gif",
+			status: "Alive"
+	},
+	{		
+			id: 3,
+			name: "Stan",
+			img_url: "https://cdn.shopify.com/s/files/1/0170/5859/4880/files/stanCharacter.png?v=1613773562",
+			status: "Alive"
+	},
+  {
+    id: 4,
+    name: "Kenny",
+    img_url: "https://i.pinimg.com/originals/80/32/90/803290c506ce5a11881ac5a010335551.jpg",
+    status: "Dead"
+  }
+]
